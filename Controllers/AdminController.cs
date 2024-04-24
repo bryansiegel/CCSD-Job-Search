5c4514ecac9846ce1082342544c95379d86fa6cb
Bryan Siegel <bryansiegel@gmail.com>
2024-04-24 16:36:49 -0700

    Login to Dashboard and scaffoled Identity

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿//using AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+
+namespace CCSD_Job_Search.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Route("/Admin/Dashboard")]
+        public IActionResult Index()
+        {
+            return View("~/Views/Admin/Dashboard.cshtml");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using CCSD_Job_Search.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CCSD_Job_Search
 {
@@ -12,13 +14,22 @@ namespace CCSD_Job_Search
 
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            //builder.Services.ConfigureApplicationCookie(options =>
+            //{
+            //    options.LoginPath = ""
+            //});
             builder.Services.AddControllersWithViews();
+
+        
 
             var app = builder.Build();
 
